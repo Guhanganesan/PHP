52c22220205b8c0cba262551b7fa1c8b85fb2900
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-28 12:52:36 +0530

    Create 1_Class.php

---
+++ OOPS/Class/1_Class.php
@@ -0,0 +1,23 @@
+<?php
+      class RBI  # [ extends super class name]
+	  {
+		  # [ fields declaration; ]
+		  # [ method declaration; ]
+		   
+		   public $name="RBI"; # Instance variable, ( no storage space has created) also known as member variable
+		   
+		   # The class with only data fields has no life. and without method that operate on that data.
+		   # Methods are necessory for manipulating the data contained in the class.
+		   # 
+		  
+	  }
+	  
+	  
+ /*  class is a user defined data type
+     1. Data items are called filed
+	 2. Functions are called methods
+	 3. variable are instance of a class. which are the actual objects.
+	 4. The keyword extedns indicates that the properties of the superclassname class are extended to the class name
+	 5. Fileds and methods are delared inside the body
+*/
+?>
