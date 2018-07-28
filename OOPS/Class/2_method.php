3f4a1ce80e4ee465af8a417b9698dba5ade2d43c
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-28 13:02:48 +0530

    Create 2_method.php

---
+++ OOPS/Class/2_method.php
@@ -0,0 +1,31 @@
+<?php
+      /*
+	      type method_name( parameter-list)
+		  {
+			  method-body;
+		  }
+		  
+		  
+	  
+	  */
+	  
+	  class RBI
+	  {
+		  function rate()
+		  {
+			  echo "Rate is 12%";
+		  }
+		  
+		  
+	  }
+	  
+	  $object = new RBI();
+	  $object->rate();
+?>
+
+
+Result:
+
+Rate is 12%
+
+
