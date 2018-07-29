c3234cb7c9c68a11f1db007533dc267156718c09
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-29 12:03:56 +0530

    Create 6_Constructor1.php

---
+++ OOPS/Class/6_Constructor1.php
@@ -0,0 +1,21 @@
+<?php
+ class Tamil
+ {
+    function __construct()
+	{
+		 $this->getValues();
+	}
+	
+	function getValues()
+	{
+		echo "Hi";
+	}
+	
+ }
+	$obj= new Tamil();
+	
+	
+?>
+
+Result: 
+    Hi
