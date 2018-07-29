8ea0730b6204fb570e7f0119862d50e6f69669ec
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-29 10:01:10 +0530

    Create 11_Without_object.php

---
+++ OOPS/Class/11_Without_object.php
@@ -0,0 +1,20 @@
+<?php
+      class Tamilan
+	  {  		  
+		  public static $name="Tamilanda";
+		  const age=20000;
+		  function veeram()
+		  {
+			print  Tamilan::$name; 
+			print  Tamilan::age; 
+		  }
+		  
+		 
+	  }
+	 Tamilan::veeram();
+?>
+
+
+Result:
+Tamilanda20000
+
