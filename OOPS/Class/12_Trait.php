c4c647f8652f16dcd213873d4b6b05ac4e6f535c
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-29 10:47:10 +0530

    Update 12_Trait.php

--- OOPS/Class/12_Trait.php
+++ OOPS/Class/12_Trait.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Traits are a mechanism for code reuse in single inheritance languages such as PHP. 
+A Trait is intended to reduce some limitations of single inheritance by enabling a developer
+to reuse sets of methods freely in several independent classes living in different class hierarchies.
+*/
     Trait Anna
 	 {
 		public function katravai()
