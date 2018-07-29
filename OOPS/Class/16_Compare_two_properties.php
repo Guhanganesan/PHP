fee6148d4ff1a9ebc72d43318fa0ab3abb35979e
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-29 11:49:30 +0530

    Create 16_Compare_two_properties.php

---
+++ OOPS/Class/16_Compare_two_properties.php
@@ -0,0 +1,21 @@
+<?php
+    class Anna{
+		
+	}
+	class Periyar
+	{
+		
+	}
+    $anna = new Anna();
+	$anna->age=87;
+	$periyar= new Periyar();
+	$periyar->age=90;
+	if( $anna->age<$periyar->age )
+	{
+		echo "True";
+	}
+?>
+
+
+Result:
+True
