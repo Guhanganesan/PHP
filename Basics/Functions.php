38073449a8bd46c1b2d39b186446cf84ae8440c3
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-24 11:14:19 +0530

    Update Functions.php

--- Basics/Functions.php
+++ Basics/Functions.php
@@ -1,51 +1,85 @@
 <?php
-$a =10; // global variable
+$a =10; 		// global variable
 $b = 20;
 function addVar(){
-	//$a = 50;
-	$x = 5; // local variable
+	$x = 5; 	// local variable
 	$y = 10;
 	$z = $x+$y;
 	global $a,$b;
 	
-	echo $a+$b;
+	echo $z."<br>";		//15
+	echo $a+$b; 		//30
 	echo "<br>";
-	echo "$a";// undefined variable
+	echo "$a<br>";		//10
 }
 addVar();
-echo $a;
-echo "<br>";
-echo $b;
+echo $a;//10
 ?>
+
+<hr>
+
 <?php
 function playCrick()
 { $a = "Sachin";
   $b = " Tendulkar";
-  echo "$a,$b<br>";
+  echo "\$a,$b<br>";//   $a, Tendulkar
 }
 playCrick(); 
 
-//
+/*-----------------------------*/
+
 function sendMessage($m)
 {
 	echo "Message sent $m";
 }
-sendMessage("Successfully!!<br>");
-//
+sendMessage("Successfully!!");
+
+/*-----------------------------*/
 
 function rate($x=12)
 {
-	echo "Today idly rate is $x<br>";
+	echo "<br> Today idly rate is  $x <br>";
 }
-rate(30);// for accessing the default value as 5
+rate(4);// for accessing the default value as 5
        // we can change the default value by assign rate(20);
-//
+/*------------------------------*/
+	   
+	   
+	   
 function run($s, $t)
-{
+{   
+    $s=40;
+	$t=5;
 	$v= $s/$t;
 	return $v;
 }   
-$w=run(20,4); // $v is a local variable
-// echo run(20,4);
-//echo "The Speed is:$w Km/hr.";
+$w=run(20,4); 
+echo "The Speed is:$w Km/hr.<br>";
+
+/*------------------------------*/
+
+
+function speed($s,$t)
+{   
+    $v= $s/$t;
+	return $v;
+}   
+$w=speed($t=4,$s=20); 
+echo "The Speed is:$w Km/hr.<br>";
+
+/*-----------------------------*/
+
 ?>
+
+
+Answers:-
+
+15
+30
+10
+10
+$a, Tendulkar
+Message sent Successfully!!
+Today idly rate is 4 
+The Speed is:8 Km/hr.
+The Speed is:0.2 Km/hr.
