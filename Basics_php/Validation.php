b35752cf12c98521d08d48ae106296c73ebcea94
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-10-22 10:31:05 +0530

    Update Validation.php

--- Basics_php/Validation.php
+++ Basics_php/Validation.php
@@ -1,54 +1,35 @@
-       
-	   
-			 <?php
-			 $a="";$b="";$x="";$y="";$m="";
-			  if(empty($_POST['name']))
+<html>
+	<form action='' method='post'>
+	  Name: <input type='text' name='x'> 
+			<br><br>
+	 Mobile:<input type='text' name='y'>
+			<br><br>
+			<input type='submit' name='submit' value='submit'/>
+	</form>          
+</html>
+<?php	
+ if(isset($_POST['submit']))
+ {			
+			  if(empty($_POST))
 			  {
-				  $a="Name must be filled out!!";
-				  
+				  echo "<script> alert('All should be filled')</script>";
+		      }
+			  else if(empty($_POST['x']))
+			  {
+				  echo "<script> alert('Name should be filled')</script>";
+			  }
+			  else if(empty($_POST['y']))
+			  {
+				  echo "<script> alert('Mobile should be filled')</script>";
 			  }
 			  else
 			  {
-				  $x=$_POST['name'];
-				  $pattern="/^[a-zA-Z]*$/";
-		          if (!preg_match($pattern,$x))
-										{
-											$m="Not-valid Name"; 
-										}
-				      				  
+				  echo "Name: ".$_POST['x']."<br>";
+				  echo "Mobile :".$_POST['y']."<br>";
+				  unset($_POST['submit']);
+				  unset($_POST['x']);
+				  unset($_POST['y']);
 			  }
 			  
-			  if(empty( $_POST['mob']))
-			  {
-				  $b="Mobile must be filled out!!";
-				  
-			  }
-			   else
-			  {
-				  $y=$_POST['mob'];
-				  
-		 	  }
-			  ?>
-			  
-			  
-			  <html>
-			<form action='Required.php' method='post'>
-			Name: <input type='text' name='name'> 
-			<span style='color:red;'><?php echo $a; echo $m; ?> </span>
-			<br><br>
-			Mob: <input type='text' name='mob'>
-			<span style='color:red;'><?php echo $b; ?> </span>
-			<br> <br>
-            <input type='submit' name='submit' value='submit'/>
-			</form>          
-            </html>
-			
-			<?php
-			      
-			echo "<b>Name:</b>".$x."<br>";
-			echo "<b>Mob:</b>".$y."<br>";
-			echo $m;
-			?>  
-			
-			
-			
+ }
+?>  
