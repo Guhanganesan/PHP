bf584f2bfb077b1cc3dad92d83f946f50120da92
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-12-01 13:40:25 +0530

    Update db.php

--- Form/db.php
+++ Form/db.php
@@ -1,3 +1,8 @@
 <?php
+/*
+db:mydb;
+query:create table user(id int auto_increment, primary key(id), name varchar(30), email varchar(30), pass varchar(30));
+*/
+
 $con=mysqli_connect("localhost","root","","mydb");
 ?>
